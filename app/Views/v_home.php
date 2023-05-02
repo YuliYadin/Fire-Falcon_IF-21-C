6e0171006d24767e2012f1e4373d5b5b2a9f6372
YuliYadin <bratheradin@gmail.com>
2023-05-02 18:25:22 +0700

    first commit

---
+++ app/Views/v_home.php
@@ -0,0 +1,67 @@
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+
+            <h1>
+                <i class="fa fa-dashboard"></i>
+                Dashboard
+                <small>Arsip 2023</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-red">
+                        <div class="inner">
+                            <h3>0</h3>
+
+                            <p>Document</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-file-pdf-o"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 col-xs-6">
+                        <!-- small box -->
+                        <div class="small-box bg-red">
+                            <div class="inner">
+                                <h3>0</h3>
+
+                                <p>Document</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-file-pdf-o"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-3 col-xs-6">
+                            <!-- small box -->
+                            <div class="small-box bg-red">
+                                <div class="inner">
+                                    <h3>0</h3>
+
+                                    <p>Document</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="fa fa-file-pdf-o"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
