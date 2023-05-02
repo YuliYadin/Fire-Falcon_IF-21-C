6f16ee357a5b134106bdb76570002b96c68ffbfd
YuliYadin <118153482+YuliYadin@users.noreply.github.com>
2023-05-02 18:39:28 +0700

    Add files via upload

---
+++ arsip-ci4 (1).sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 09 Apr 2023 pada 18.50
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `arsip-ci4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_arsip`
+--
+
+CREATE TABLE `tbl_arsip` (
+  `id_arsip` int(11) NOT NULL,
+  `id_kategori` int(11) DEFAULT NULL,
+  `no_arisip` varchar(10) DEFAULT NULL,
+  `nama_file` varchar(255) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `tgl_upload` date DEFAULT NULL,
+  `tgl_update` date DEFAULT NULL,
+  `file_arsip` varchar(255) DEFAULT NULL,
+  `id_dep` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_departemen`
+--
+
+CREATE TABLE `tbl_departemen` (
+  `id_departemen` int(11) NOT NULL,
+  `nama_departemen` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `level` int(1) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `id_dep` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `email`, `password`, `level`, `foto`, `id_dep`) VALUES
+(1, 'Muhammad Arif Rivai', 'arifrivai@gmail.com', '1234', 1, 'user1.jpg', 1),
+(2, 'Yuli Adin', 'yuliadin@gmail.com', '1234', 1, 'user2.jpeg', 2),
+(3, 'Rio Efendi', 'rioefendi@gmail.com', '1234', 1, 'user3.jpeg', 3),
+(4, 'Aditya Saputra', 'adityasaputra@gmail.com', '1234', 1, 'user4.jpg', 4),
+(5, 'Arif Aditian Hanuza', 'arifaditian@gmail.com', '1234', 1, 'user5.jpg', 5),
+(6, 'M. Rafly Dwi Putra Iswadi', 'mrafly@gmail.com', '1234', 1, 'user6.png', 6),
+(7, 'Aldi Prasetya', 'aldi@gmail.com', '1234', 1, 'user7.jpg', 7);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_arsip`
+--
+ALTER TABLE `tbl_arsip`
+  ADD PRIMARY KEY (`id_arsip`);
+
+--
+-- Indeks untuk tabel `tbl_departemen`
+--
+ALTER TABLE `tbl_departemen`
+  ADD PRIMARY KEY (`id_departemen`);
+
+--
+-- Indeks untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_arsip`
+--
+ALTER TABLE `tbl_arsip`
+  MODIFY `id_arsip` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_departemen`
+--
+ALTER TABLE `tbl_departemen`
+  MODIFY `id_departemen` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
